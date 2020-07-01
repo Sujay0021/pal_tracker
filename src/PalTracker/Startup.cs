21efe33699aa9a0671aef1e08da65cb9e89455af
Sujay0021 <Sujay_Bheemana@Dell.com>
2020-07-01 06:01:38 +0000

    TimeControllerAPI

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -40,7 +40,7 @@ namespace PalTracker
            }
            services.AddSingleton(sp => new WelcomeMessage(message));
            services.AddSingleton( sp => new CloudFoundryInfo(port,memory,index,addr));
-
+           services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
